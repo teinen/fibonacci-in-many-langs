1ef10ce2139a205a6e35a7f279efbeb7c3115ef1
諦念 <teinen.github@gmail.com>
2018-08-01 00:28:05 +0900

    Memoize and more simple pattern in Ruby

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,19 @@
-# fibonacci func
+# use memoization
+@values = Hash.new
+
 def fib(n)
+  return @values[n] if @values[n]
   return n if n <= 1
-  return fib(n - 1) + fib(n - 2)
+  return @values[n] = fib(n - 1) + fib(n - 2)
 end
 
-10.times do |i|
+100.times do |i|
   puts fib(i)
+end
+
+# not use memoization
+a, b = 0, 1
+100.times do
+  puts a
+  a, b = b, a + b
 end
