336197f68a63b0b213db6aa151808f7ca6de26cc
諦念 <teinen.github@gmail.com>
2018-07-31 23:38:09 +0900

    Minifiy implementation in Java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,20 +7,14 @@ public class Fibonacci {
     // memoize
     Map<Integer, BigInteger> values = new HashMap<>();
 
-    System.out.println(fibonacci(100, values));
+    System.out.println(fibonacci(50, values));
   }
 
   public static BigInteger fibonacci(Integer n, Map<Integer, BigInteger> values) {
-    if (values.containsKey(n)) {
-      return values.get(n);
-
-    } else if (n == 0) {
-      return BigInteger.ZERO;
-
-    } else if (n == 1) {
-      return BigInteger.ONE;
-
-    } else {
+    if (values.containsKey(n)) return values.get(n);
+    else if (n == 0) return BigInteger.ZERO;
+    else if (n == 1) return BigInteger.ONE;
+    else {
       BigInteger fib = fibonacci(n - 1, values).add(fibonacci(n - 2, values));
       values.put(n, fib);
       return fib;
