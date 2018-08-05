827e8092e33fb9ad6ef581436460dd93882d989f
諦念 <teinen.github@gmail.com>
2018-08-05 15:06:46 +0900

    Refactor java implementation

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,19 +3,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Fibonacci {
-  public static void main(String[] args) {
-    // memoize
-    Map<Integer, BigInteger> values = new HashMap<>();
 
-    System.out.println(fibonacci(50, values));
+  // memoize
+  private static Map<Integer, BigInteger> values = new HashMap<>();
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 50; i++) {
+      System.out.println(fibonacci(i));
+    }
   }
 
-  public static BigInteger fibonacci(Integer n, Map<Integer, BigInteger> values) {
+  public static BigInteger fibonacci(Integer n) {
     if (values.containsKey(n)) return values.get(n);
     else if (n == 0) return BigInteger.ZERO;
     else if (n == 1) return BigInteger.ONE;
     else {
-      BigInteger fib = fibonacci(n - 1, values).add(fibonacci(n - 2, values));
+      BigInteger fib = fibonacci(n - 1).add(fibonacci(n - 2));
       values.put(n, fib);
       return fib;
     }
