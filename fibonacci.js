8de909efe797e8af6a00845acd1590506c6f8781
諦念 <teinen.github@gmail.com>
2018-08-05 15:10:02 +0900

    Refactor js implementation

--- fibonacci.js
+++ fibonacci.js
@@ -3,8 +3,7 @@ var fibs = {}
 
 // fibonacci function
 function fib(n) {
-  if (n === 0) return 0
-  if (n === 1) return 1
+  if (n <= 1) return n
   if (fibs[n]) return fibs[n]
   return fibs[n] = fib(n-1) + fib(n-2)
 }
